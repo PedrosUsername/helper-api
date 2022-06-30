16ba0240c5964969551f030c5fb509bc7fbdc154
Pedro <pilique11@gmail.com>
2022-06-30 00:54:28 -0300

    add outros testes

--- src/services/ArrayService.ts
+++ src/services/ArrayService.ts
@@ -2,7 +2,20 @@ export class ArrayService {
   constructor() {}
 
   tratarArrayEx002 = (arr: string[] | number[], sort: string, allowDuplicates: string) => {
-    
-    return arr.sort();
+    let result;
+
+    if(allowDuplicates === 'no'){
+      result = [...new Set(Array.of(...arr))];
+    } else {
+      result = arr;
+    }
+
+    if(sort === 'desc'){
+      result = result.sort().reverse();
+    } else {
+      result = result.sort()
+    }
+
+    return result;
   };
 }
--- tests/ArrayService/tratarArrayEx002.test.ts
+++ tests/ArrayService/tratarArrayEx002.test.ts
@@ -1,19 +1,46 @@
 import { ArrayService } from "../../src/services/ArrayService";
 
 let service = new ArrayService;
+const NUMBERS_ARRAY = [9, 8, 7, 6, 6];
+const WORDS_ARRAY = ["zebra", "jaime", "alemao", "jaime"];
 
 test('deve instanciar service', () => {
     service = new ArrayService;
 });
 
+
+
+
 test('ex002 deve poder ordenar array vazio', () => {
     expect(service.tratarArrayEx002([], 'asc', 'yes')).toStrictEqual([]);
 });
 
+
+
+
+
 test('ex002 deve poder ordenar array de numeros asc', () => {
-    expect(service.tratarArrayEx002([9, 8, 7, 6], 'asc', 'yes')).toStrictEqual([6, 7, 8, 9]);
+    expect(service.tratarArrayEx002(NUMBERS_ARRAY, 'asc', 'yes')).toStrictEqual([6, 6, 7, 8, 9]);
 });
 
-test('ex002 deve poder ordenar array de strings', () => {
-    expect(service.tratarArrayEx002(['zebra', 'jaime', 'alemao'], 'asc', 'yes')).toStrictEqual(['alemao', 'jaime', 'zebra']);
+test('ex002 deve poder ordenar array de numeros desc', () => {
+    expect(service.tratarArrayEx002(NUMBERS_ARRAY, 'desc', 'yes')).toStrictEqual([9, 8, 7, 6, 6]);
+});
+
+test('ex002 deve poder ordenar array de numeros desc sem repeticoes', () => {
+    expect(service.tratarArrayEx002(NUMBERS_ARRAY, 'desc', 'no')).toStrictEqual([9, 8, 7, 6]);
+});
+
+
+
+test('ex002 deve poder ordenar array de strings asc', () => {
+    expect(service.tratarArrayEx002(WORDS_ARRAY, 'asc', 'yes')).toStrictEqual(['alemao', 'jaime', 'jaime', 'zebra']);
+});
+
+test('ex002 deve poder ordenar array de strings desc', () => {
+    expect(service.tratarArrayEx002(WORDS_ARRAY, 'desc', 'yes')).toStrictEqual(['zebra', 'jaime', 'jaime', 'alemao']);
+});
+
+test('ex002 deve poder ordenar array de strings desc sem repeticoes', () => {
+    expect(service.tratarArrayEx002(WORDS_ARRAY, 'desc', 'no')).toStrictEqual(['zebra', 'jaime', 'alemao']);
 });
