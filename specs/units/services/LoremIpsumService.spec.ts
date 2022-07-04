cba39b25db96c7d94cc41dbe216df39d9915e5b1
Pedro <pilique11@gmail.com>
2022-07-03 21:08:40 -0300

    add test (vermelho)

--- specs/units/services/LoremIpsumService.spec.ts
+++ specs/units/services/LoremIpsumService.spec.ts
@@ -7,15 +7,39 @@ describe('Executando teste na classe LoremIpsumService', () => {
     _service = new LoremIpsumService()
   })
 
-  it ('Deveria retonar uma string com 4 palavras', () => {
+  it ('Deveria retonar uma string com 5 palavras', () => {
     // Arrange
-    const quantidade = 4
+    const quantidade = 5
 
     // Act
-    const lorenIpsumText = _service.generate(4)
+    const lorenIpsumText = _service.generate(5)
     const result = lorenIpsumText.split(" ").length;
 
     // Assert
     expect(result).toBe(quantidade)
   })
+
+  it ('Deveria retonar uma string com 69 palavras', () => {
+    // Arrange
+    const quantidade = 69
+
+    // Act
+    const lorenIpsumText = _service.generate(69)
+    const result = lorenIpsumText.split(" ").length;
+
+    // Assert
+    expect(result).toBe(quantidade)
+  })
+
+  it ('Deveria retonar uma string com 75 palavras', () => {
+    // Arrange
+    const quantidade = 75
+
+    // Act
+    const lorenIpsumText = _service.generate(75)
+    const result = lorenIpsumText.split(" ").length;
+
+    // Assert
+    expect(result).toBe(quantidade)
+  })    
 })
